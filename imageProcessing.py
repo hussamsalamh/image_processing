e6369c640491b5e51aa9de8ba2a172a4b1baddde
hussamsalamh <77392590+hussamsalamh@users.noreply.github.com>
2021-08-13 19:44:16 +0300

    Update imageProcessing.py

--- imageProcessing.py
+++ imageProcessing.py
@@ -2,8 +2,16 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-NUM_CHAPTER = 8
-NUM_QUESTION = 25
+from studentInfo import studentInfo, Chapter
+
+TYPE_ERROR = 'Please check the type of each chapter \n'
+ID_LENGTH = 8
+
+
+numQuestion = {'M': 20,
+               'A': 20,
+               'E': 22,
+               'P': 0}
 
 """
 
@@ -23,7 +31,6 @@ the mathmaitical grade
 """
 
 
-
 def get_min_xy(img):
     min_x, max_y = 0, 0
     for x in range(img.shape[1]):
@@ -36,35 +43,25 @@ def get_min_xy(img):
             break
     return min_x, max_y
 
-# still need to do the calculation
-class studentInfo:
-    def __init__(self,name,id,math,english,arabic):
-        self.name = name
-        self.id = id
-        self.studentMistake = dict()
-        self.math = math
-        self.eng = english
-        self.ar = arabic
+
+# type of chapter will be like that
+"""
 
 
-    def addGrade(self,chapterNumber,numMistake):
-        self.studentMistake[chapterNumber] = numMistake
-
-    def calcualteMathematicalGrade(self):
-        grade = 0
-
+"""
 
 
 class imageProcessing:
-    def __init__(self, path, folder,unmarkChapter):
-        self.unmarkChapter = unmarkChapter
-        self.images = []
-        self.img = path
+    def __init__(self, path, folder, typeChapter):
+        self.typeChapter = typeChapter  # this gonna be string EAMP -> english arabic math unmarked
+        # type of chapter the length must be 8 else raising exception
+
+        self.studentsImagePaper = []
+        self.teacherImage = path
         self.folder = folder
         self.correctAnswer = []
-        self.studentAnswer=[]
-        self.students = []
-        self.studentsGrade = dict()
+        self.studentAnswer = []
+        self.studentsResult = []
         self.global_cache = dict()
 
     def list_bars(self, img):
@@ -129,8 +126,9 @@ class imageProcessing:
         result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR, borderValue=fill)
         return result
 
-    def process_image(self,img):
-        img = cv2.imread(img)
+    def process_image(self, img, des=True):
+        if des:
+            img = cv2.imread(img)
         if img.shape[0] > img.shape[1]:
             img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -146,15 +144,21 @@ class imageProcessing:
         img_rot = self.rotate_image(thresh, -angle)
         min_x, max_y = get_min_xy(img_rot)
         img_cropped = img_rot[:max_y - 5, min_x + 5:]
-        plt.imshow(img_cropped, cmap='gray')
-        plt.show()
+        # plt.imshow(img_cropped, cmap='gray')
+        # plt.show()
         self.global_cache['x_off'] = min_x + 5
         self.global_cache['y_off'] = max_y + 5
 
         y_bars, x_bars = self.list_bars(img_cropped)
         return img_cropped, y_bars, x_bars
 
-    def get_ans(img, chapter_bars, question_bars, chapter, question):
+    # Still need to handle two answer in col
+
+    '''
+    This function return the index of the the answer in the sheet
+    '''
+
+    def get_ans(self, img, chapter_bars, question_bars, chapter, question):
         if chapter > 5:
             chapter -= 3
             question += 30
@@ -165,10 +169,7 @@ class imageProcessing:
             ans[i - 1] = np.sum(img[y1:y2, x1:x2])
         if np.all(ans > ans.max() / 2):
             return None
-        arr = ans.tolist()
-        min_value = min(arr)
-        min_index = arr.index(min_value)
-        return min_index
+        return np.argmin(ans)
 
     def draw_ans(self, img, chapter_bars, question_bars, chapter, question, ans):
         t = self.global_cache['angle']
@@ -186,80 +187,126 @@ class imageProcessing:
         center = center[0] + x_off, center[1]
         cv2.ellipse(img, center, ellipse_size, 0, 0, 360, (0, 255, 0), -1)
 
-    def rotate_point(self,x, y, t):
+    def rotate_point(self, x, y, t):
         t = np.radians(t)
         xx = x * np.cos(t) - y * np.sin(t)
         yy = x * np.sin(t) + y * np.cos(t)
         return int(xx), int(yy)
 
-    def id(self,img, y_arr, x_axis, j):
-        id_arr = np.zeros((9,))
-        for i in range(9):
-            y1, y2 = y_arr[i]
-            x1, x2 = x_axis[39 + j]
-            id_arr[i - 1] = np.sum(img[y1:y2, x1:x2])
+    def id(self, img, y_arr, x_axis, j):
+        id_arr = np.zeros((10,))
+        for i in range(10):
+            y1, y2 = y_arr[j]
+            x1, x2 = x_axis[39 + i]
+            id_arr[i] = np.sum(img[y1:y2, x1:x2])
         if np.all(id_arr > id_arr.max() / 2):
             return None
+        return 9 - np.argmin(id_arr)
 
-    def take_id(self,img, y_axis, x_axis):
+    def getStudentId(self, img, y_axis, x_axis):
         diff = y_axis[1][0] - y_axis[0][1]
         h = y_axis[0][1] - y_axis[0][0]
         end = y_axis[0][1]
         start = y_axis[0][0]
         y_arr = []
-        for i in range(9):
+        for i in range(ID_LENGTH):
             y_arr.append((start, end))
             end = start - diff
             start = end - h
         id_arr = []
-        for j in range(9):
-            id_arr.append(self.id(img, y_arr, x_axis, j))
-        print(id_arr)
-        return id_arr
+        for j in range(ID_LENGTH):
+            num = self.id(img,y_arr,x_axis,j)
+            id_arr.insert(0,num)
+        id_str = ''.join(str(num) for num in id_arr)
+        return id_str
 
     def getCorrectAnswer(self):
-        img, chapter_bars, question_bars = self.process_image(self.img)
-        res = self.rotate_image(cv2.imread(self.img), -self.global_cache['angle'])
-        for chapter in range(1, 8 + 1):
-            for question in range(1, 25 + 1):
-                self.correctAnswer[chapter].apppend(self.get_ans(img, chapter_bars, question_bars, chapter, question))
-                # if ans is not None:
-                #     self.draw_ans(res, chapter_bars, question_bars, chapter, question, ans)
+        img, chapter_bars, question_bars = self.process_image(self.teacherImage)
+        res = self.rotate_image(cv2.imread(self.teacherImage), -self.global_cache['angle'])
+        for chapter in range(1, len(self.typeChapter) + 1):
+            if self.typeChapter[chapter - 1] == Chapter.unmarked.value:
+                continue
+            arr = []
+            for question in range(1, numQuestion[self.typeChapter[chapter - 1]] + 1):
+                ans = self.get_ans(img, chapter_bars, question_bars, chapter, question)
+                if ans is None:
+                    arr.append(-1)
+                else:
+                    arr.append(ans)
+                    # self.draw_ans(res, chapter_bars, question_bars, chapter, question, ans)
+            self.correctAnswer.append(arr)
         # cv2.imwrite('detected.png', res)
         # plt.imshow(res, cmap='gray')
         # plt.show()
-
-    # def findIdStudent(self):
-
 
     def uploadImages(self):
         for filename in os.listdir(self.folder):
             img = cv2.imread(os.path.join(self.folder, filename))
             if img is not None:
-                self.images.append(img)
+                self.studentsImagePaper.append(img)
 
-
-    def getStudentAnswer(self,image):
-        img, chapter_bars, question_bars = self.process_image(image)
-        res = self.rotate_image(cv2.imread(self.img), -self.global_cache['angle'])
-        for chapter in range(1, NUM_CHAPTER+ 1):
-            arr = []
-            for question in range(1, NUM_QUESTION + 1):
-                arr.apppend(self.get_ans(img, chapter_bars, question_bars, chapter, question))
-            self.studentAnswer.append(arr)
-
-    def getStudentGrade(self,studentId):
-        chapters = dict()
-        student = studentInfo(self.)
-        for i in range(NUM_CHAPTER):
-            if i +1 in self.unmarkChapter:
+    def getStudentAnswer(self, image):
+        img, chapter_bars, question_bars = self.process_image(image,False)
+        res = self.rotate_image(cv2.imread(self.teacherImage), -self.global_cache['angle'])
+        id = self.getStudentId(img, chapter_bars, question_bars)
+        for chapter in range(1, len(self.typeChapter) + 1):
+            if self.typeChapter[chapter - 1] == Chapter.unmarked.value:
                 continue
-            for j in range(NUM_QUESTION):
-                if self.studentAnswer[i][j] != self.correctAnswer[i][j]:
+            arr = []
+            for question in range(1, numQuestion[self.typeChapter[chapter - 1]] + 1):
+                ans = self.get_ans(img, chapter_bars, question_bars, chapter, question)
+                if ans is None:
+                    arr.append(-1)
+                else:
+                    self.draw_ans(res, chapter_bars, question_bars, chapter, question, ans)
+                    arr.append(ans)
+            self.studentAnswer.append(arr)
+        return id
 
 
 
+    def getStudentGrade(self, studentId):
+        student = studentInfo(studentId, self.typeChapter)
+        index = -1
+        for chapter in range(len(self.typeChapter)):
+            if self.typeChapter[chapter] == Chapter.unmarked.value:
+                continue
+            index += 1
+            for question in range(numQuestion[self.typeChapter[chapter]]):
+                try:
+                    if self.studentAnswer[index][question] \
+                            != self.correctAnswer[index][question]:
+                        student.incrementMistake(chapter)
+                except:
+                    print(chapter, question)
+        self.studentsResult.append(student)
 
 
+    #TODO : need to install the library to make the file that contain the info
+    # of students
+    """
+    need to think how to get the name of the students 
+    without reading the file again 
+    
+    """
 
-# if __name__ == '__main__':
+    def createExcel(self):
+
+        for student in self.studentsResult:
+            print(student.getId())
+            print(student.getGrade())
+
+    def doAction(self):
+        self.getCorrectAnswer()
+        self.uploadImages()
+        for i in range(len(self.studentsImagePaper)):
+            studentId = self.getStudentAnswer(self.studentsImagePaper[i])
+            self.getStudentGrade(studentId)
+        self.createExcel()
+
+
+if __name__ == '__main__':
+    pro = imageProcessing('/cs/usr/hussamsal/PycharmProjects/image/image.jpeg'
+                          , '/cs/usr/hussamsal/PycharmProjects/image/output_Image',
+                          'MEEMEPAP')
+    pro.doAction()
